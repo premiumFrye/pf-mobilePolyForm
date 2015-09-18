f3a63cb3194233d527d18b6e7ae51ec927346d92
Nicholas Frye <nick@mobileigniter.com>
2015-09-18 12:45:00 -0500

    refactor - handle selects along with inputs; auto advance cursor after selecting from list

--- pf-mobilePolyForm.js
+++ pf-mobilePolyForm.js
@@ -7,8 +7,12 @@ angular.module('pf-mobilePolyForm', [])
     var inputs = [],
     // any other methods declared in template for ng-keydown that we'll want called 
       keydownFns = [],
-      //submit our form when we mean to
-      formSubmit;
+      focusInput = function (n) {
+        $timeout(function () {
+        // move cursor to next input field
+          if (inputs[n]) { inputs[n][0].focus(); }
+        }, 0, false);
+      };
 
     return {
       require: 'form',
@@ -25,33 +29,31 @@ angular.module('pf-mobilePolyForm', [])
               keydownFns[inputs.length] = $parse(thisInput.attr('ng-keydown'));
               inputs.push(thisInput.attr('ng-keydown', 'nextInput($event,' + inputs.length + ');'));
             }
-          }
-          if (formElements[eNum].nodeName === 'SELECT') {
-            var thisSelect = angular.element(formElements[eNum])[0];
-            // ng-model doesn't always update (browser discrepancies) and selected option doesn't always show as selected with certain browsers - throw a shim in there.
-            thisSelect.onchange = function () {
-              thisSelect.blur();
-            };
-            // make android act like iOS - when user hits 'return' and focuses on a select field, emulate a mouse click and open up options
-            if (ionic.Platform.isAndroid()) {
-              thisSelect.onfocus = function () {
-                if (document.createEvent) {
-                  var e = document.createEvent("MouseEvents");
-                  e.initMouseEvent("mousedown", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-                  thisSelect.dispatchEvent(e);
-                } else if (thisSelect.fireEvent) {
-                  thisSelect.fireEvent("onmousedown");
-                }
+            if (formElements[eNum].nodeName === 'SELECT') {
+              thisInput = thisInput[0];
+              // ng-model doesn't always update (browser discrepancies) and selected option doesn't always show as selected with certain browsers - throw a shim in there.
+              thisInput.onchange = function () {
+                thisInput.blur();
+                focusInput(parseInt(eNum) + 1);
               };
+              // make android act like iOS - when user hits 'return' and focuses on a select field, emulate a mouse click and open up options
+              if (ionic.Platform.isAndroid()) {
+                thisInput.onfocus = function () {
+                  if (document.createEvent) {
+                    var e = document.createEvent("MouseEvents");
+                    e.initMouseEvent("mousedown", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+                    thisInput.dispatchEvent(e);
+                  } else if (thisInput.fireEvent) {
+                    thisInput.fireEvent("onmousedown");
+                  }
+                };
+              }
             }
           }
-
         });
         return {
 
-          post: function (scope, elm, attr) {
-            angular.noop(elm);
-            formSubmit =  $parse(attr.ngSubmit);
+          post: function (scope) {
             scope.nextInput = function (e, num) {
               // apply any functions user had attached to ng-keydown on input
               if (keydownFns[num]) { keydownFns[num](scope, {$event: e}); }
@@ -60,10 +62,7 @@ angular.module('pf-mobilePolyForm', [])
                 if (e.keyCode === 13) {
                   // if we're not on the last input, don't submit the form!                  
                   if (inputs.length !== num) { e.preventDefault(); }
-                  $timeout(function () {
-                  // move cursor to next input field
-                    if (inputs[num + 1]) { inputs[num + 1][0].focus(); }
-                  }, 0, false);
+                  focusInput(num + 1);
                 }
               }
             };
